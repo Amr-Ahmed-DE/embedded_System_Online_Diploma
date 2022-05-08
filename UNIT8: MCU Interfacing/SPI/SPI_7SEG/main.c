f9b13a9381c62e5b9831e63232f87179f9562173
amr ahmed <80995017+Amr-Ahmed-DE@users.noreply.github.com>
2022-05-08 23:45:06 +0200

    Add files via upload

---
+++ UNIT8: MCU Interfacing/SPI/SPI_7SEG/main.c
@@ -0,0 +1,27 @@
+/*
+ * SPI_7SEG.c
+ *
+ * Created: 08/05/2022 23:30:58
+ * Author : AMR AHMED
+ */ 
+
+#include <avr/io.h>
+#include "MCAL/SPI_DRIVER/SPI.h"
+
+int main(void)
+{
+    unsigned char counter = 0;
+    unsigned char index_display;
+    SPI_MasterInit();
+    MAX_execute(0x09, 0xFF);
+    MAX_execute(0x0A, 0xFF);
+    MAX_execute(0x0B, 0xF7); //scan limit register format B :: 0xX7
+    MAX_execute(0x0C, 0x01);
+    while (1)
+    {
+	    for (index_display = 1; index_display < 9; index_display++)
+	    MAX_execute(index_display, counter++);
+	    _delay_ms(1000);
+    }
+}
+
